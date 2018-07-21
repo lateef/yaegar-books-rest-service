d6eae2ffb07a81a652dd6d5141d25c5bd8859c0e
Lateef Adeniji-Adele <adenijiadele@yahoo.com>
2018-07-21 19:41:48 +0100

    first commit

---
+++ src/main/java/com/yaegar/yaegarbooksrestservice/YaegarBooksRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yaegar.yaegarbooksrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaegarBooksRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaegarBooksRestServiceApplication.class, args);
+    }
+}
